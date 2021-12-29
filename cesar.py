b8641d7afe32a9547c3c79fdb14962a555899695
Anonymkus <90962002+Anonymkus@users.noreply.github.com>
2021-12-29 17:39:29 +0300

    Add files via upload

---
+++ cesar.py
@@ -0,0 +1,25 @@
+alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+alphabet += alphabet.upper()
+
+
+
+def encrypt(message:str, shift:int) -> str:
+	result = ""
+	for letter in message:
+		if letter in alphabet:
+			index = alphabet.index(letter)
+			index_shifted = (index + shift) % len(alphabet)
+			result += alphabet[index_shifted]
+		else:
+			result += letter
+	return result
+
+message_encrypted = encrypt("Привет", 5)
+
+def bruteforce_encrypt(message:str, func) -> str:
+	for i in range(len(alphabet)):
+		print(i, encrypt(message_encrypted, i * -1))
+
+
+bruteforce_encrypt(message_encrypted, encrypt)
+
